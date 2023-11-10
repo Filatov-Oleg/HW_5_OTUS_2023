b17835026f11f9efa3a1bf89843c32ceac465f2c
Filatov <o.filatov@rendez-vous.ru>
2023-11-10 18:24:04 +0300

    Added README

---
+++ HW_5_OTUS_2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HW_5_OTUS_2023
+//
+//  Created by Филатов Олег Олегович on 10.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW_5_OTUS_2023/HW_5_OTUS_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_5_OTUS_2023App.swift
+//  HW_5_OTUS_2023
+//
+//  Created by Филатов Олег Олегович on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW_5_OTUS_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
